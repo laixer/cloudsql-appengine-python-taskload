e7d9c4dbd9118c8a7081ee02f3a4bdc04dbe3b2e
Vadim Berezniker <vadim@berezniker.com>
2016-03-09 01:06:58 -0800

    Update with multiple queues.

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from google.appengine.ext import deferred
 def getDbConnection():
 	return MySQLdb.connect(unix_socket='/cloudsql/foo:bar', user='root', db='library')
 
+
 def background_work():
 	logging.info('Received task, connecting to database.')
 	with closing(getDbConnection()) as db:
@@ -26,17 +27,23 @@ def background_work():
 	logging.info('Finished processing task.')
 
 
+def schedule_work(queue, num_tasks):
+	for i in range(num_tasks):
+		deferred.defer(background_work, _queue=queue)
+
+
 class SheduleOnePage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-type'] = 'text/plain'
-		deferred.defer(background_work, _queue='test')
+		deferred.defer(schedule_work, 'q1', 1)
 		self.response.write('Single task scheduled.')
 
 class ScheduleManyPage(webapp2.RequestHandler):
 	def get(self):
+		logging.info('Scheduling tasks.')
 		self.response.headers['Content-type'] = 'text/plain'
-		for i in range(10000):
-			deferred.defer(background_work, _queue='test')
+		for q in range(1, 11):
+			deferred.defer(schedule_work, 'q%s' % q, 500)
 		self.response.write('Many tasks scheduled.')
 
 app = webapp2.WSGIApplication([
